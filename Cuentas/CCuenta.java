922ab81b0e34ff0f5e85dbf1213070b318047e54
Naomi2109 <naomi.gamba21@gmail.com>
2024-02-12 23:10:45 +0000

    cambios

--- Cuentas/CCuenta.java
+++ Cuentas/CCuenta.java
@@ -12,21 +12,21 @@ public class CCuenta {
 
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+        setNombre(nom);
+        setCuenta(cue);
+        setSaldo(sal);
     }
 
     public double estado()
     {
-        return saldo;
+        return getSaldo();
     }
 
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
+        setSaldo(getSaldo() + cantidad);
     }
 
     public void retirar(double cantidad) throws Exception
@@ -35,6 +35,38 @@ public class CCuenta {
             throw new Exception ("No se puede retirar una cantidad negativa");
         if (estado()< cantidad)
             throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - cantidad;
+        setSaldo(getSaldo() - cantidad);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getTipoInteres() {
+        return tipoInteres;
+    }
+
+    public void setTipoInteres(double tipoInteres) {
+        this.tipoInteres = tipoInteres;
     }
 }
